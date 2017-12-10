f620f4014b083f7a2faa894346c91dbdc752e30b
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2017-12-10 03:12:12 +0100

    2 boutons

--- Interface/interface.c
+++ Interface/interface.c
@@ -2,7 +2,7 @@
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include "interface.h"
 
-GtkWidget       *window, *window2, *ContinueButton2;
+GtkWidget       *window, *window2;
 GtkBuilder      *builder;
 GtkFileChooser  *selectImage;
 
@@ -12,16 +12,15 @@ int main(int argc, char *argv[])
 
   builder = gtk_builder_new();
   gtk_builder_add_from_file (builder, "window_main.glade", NULL);
- 
+
   window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
   window2 = GTK_WIDGET(gtk_builder_get_object(builder, "window_2"));
-  ContinueButton2 = GTK_WIDGET(gtk_builder_get_object(builder, "ContinueButton2"));
   selectImage = GTK_FILE_CHOOSER(gtk_builder_get_object(builder, "selectImage"));
-  
+
   gtk_builder_connect_signals(builder, NULL);
- 
+
   //g_object_unref(builder);
- 
+
   gtk_widget_show(window);                
   gtk_main();
  
@@ -44,9 +43,16 @@ void on_ContinueButton_clicked() {
   gtk_widget_show(window2);
 }
 
-void UnlockButton() {
-  gtk_widget_set_sensitive(ContinueButton2, TRUE);
+void UnlockGreyScaleButton() {
+  GtkWidget *GreyScale_button = GTK_WIDGET(gtk_builder_get_object(builder, "grey_scale_button"));
+  gtk_widget_set_sensitive(GreyScale_button, TRUE);
 }
+
+void UnlockBinarizationButton() {
+  GtkWidget *Binarization_button = GTK_WIDGET(gtk_builder_get_object(builder, "binarization_button"));
+  gtk_widget_set_sensitive(Binarization_button, TRUE);
+}
+
 
 GdkPixbuf *ResizeImage(GdkPixbuf *pixbuf)
 {
@@ -73,17 +79,50 @@ void DisplayImage(GtkFileChooser *chooser, GtkImage *image) {
   gtk_file_chooser_set_filename(chooser, path);
 }
 
-void Start_Preprocess(GtkButton *button, GtkFileChooser *selectImage) {
-  gtk_button_set_label(button, "Continuer");
-  //gtk_widget_hide(window2);
-  //gtk_widget_show(window3);
+void GreyScale(GtkButton *button, GtkFileChooser *selectImage) {
+  gtk_button_set_label(button, "Grey Scale");
   char *path = gtk_file_chooser_get_filename(selectImage);
   SDL_Surface* image = load_image(path);
   image = black_white(grey_level(image));
   SDL_SaveBMP(image, "image_1.bmp");
   GtkImage *image1 = GTK_IMAGE(gtk_builder_get_object(builder, "Image"));
-  //builder = gtk_builder_new();
   gtk_image_set_from_file(image1, "image_1.bmp");
-  //struct vector* res = GetVector(path);
+
+  //Resize
+  GdkPixbuf *pixbuf = gtk_image_get_pixbuf(image1);
+  GdkPixbuf *new_pixbuf = ResizeImage(pixbuf);
+  gtk_image_set_from_pixbuf(image1, new_pixbuf);
+
+  //g_object_unref(pixbuf);
+  g_object_unref(new_pixbuf);
+  SDL_FreeSurface(image);
+}
+
+SDL_Surface* detection_gtk(struct matrix* matrix, SDL_Surface* image) {
+  image = lines(image, 0);
+  image = columns(matrix, image);
+  return image;
+}
+void Binarization(GtkButton *button, GtkImage *img) {
+  gtk_button_set_label(button, "Binarization");
+  SDL_Surface* image = load_image("image_1.bmp");
+  struct matrix* res = binarization(image);
+  image = detection_gtk(res, image);
+
+  SDL_SaveBMP(image, "image_1.bmp");
+  gtk_image_set_from_file(img, "image_1.bmp");
+
+
+  //Resize
+  GdkPixbuf *pixbuf = gtk_image_get_pixbuf(img);
+  GdkPixbuf *new_pixbuf = ResizeImage(pixbuf);
+  gtk_image_set_from_pixbuf(img, new_pixbuf);
+
+  //struct vector *vect = blocks_detection(matrix, image);
+
   //free_vector(res);
+  //g_object_unref(pixbuf);
+  g_object_unref(new_pixbuf);
+  free_matrix(res);
+  SDL_FreeSurface(image);
 }
--- Interface/interface.h
+++ Interface/interface.h
@@ -13,4 +13,14 @@
 #include "../Preprocess/text_blocks_detection.c"
 #include "../Preprocess/mainFunctions.c"
 
+void Quit();
+void on_ContinueButton_clicked();
+void UnlockGreyScaleButton();
+void UnlockBinarizationButton();
+GdkPixbuf *ResizeImage(GdkPixbuf *pixbuf);
+void DisplayImage(GtkFileChooser *chooser, GtkImage *image);
+void GreyScale(GtkButton *button, GtkFileChooser *selectImage);
+SDL_Surface* detection_gtk(struct matrix* matrix, SDL_Surface* image);
+void Binarization(GtkButton *button, GtkImage *img);
+
 #endif
